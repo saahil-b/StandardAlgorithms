4db9d360a3b0f6707fd65de8227454abceb4210a
Saahil <bahadur.s@etoncollege.org.uk>
2020-11-11 11:16:01 +0000

    initial commit

---
+++ StandardAlgorithms/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  StandardAlgorithms
+//
+//  Created by Bahadur, Saahil (PAH) on 11/11/2020.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+let a = 5
+print("The value of a is \(a)")
